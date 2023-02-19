02024f32cca6737d7b04bacc88ea4e4c4c2f7217
lizhi <lizhiwj2019@outlook.com>
2023-02-19 22:15:02 +0800

    load gl proc

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,94 @@
-int main(){
+#include "glad/gl.h"
 
+#include <cstdio>
+// must include whole windows head file?
+#include <Windows.h>
+#include <wingdi.h>
+
+#pragma comment (lib, "opengl32.lib")
+
+GLADapiproc getproc(const char* name) {
+    auto p = (GLADapiproc)wglGetProcAddress(name);
+    if (p == 0 ||
+        (p == (void*)0x1) || (p == (void*)0x2) || (p == (void*)0x3) ||
+        (p == (void*)-1)) {
+        HMODULE module = LoadLibraryA("opengl32.dll");
+        if (module != NULL)
+            p = (GLADapiproc)GetProcAddress(module, name);
+    }
+
+    return p;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_CREATE:
+    {
+        PIXELFORMATDESCRIPTOR pfd =
+        {
+            sizeof(PIXELFORMATDESCRIPTOR),
+            1,
+            PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
+            PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
+            32,                   // Colordepth of the framebuffer.
+            0, 0, 0, 0, 0, 0,
+            0,
+            0,
+            0,
+            0, 0, 0, 0,
+            24,                   // Number of bits for the depthbuffer
+            8,                    // Number of bits for the stencilbuffer
+            0,                    // Number of Aux buffers in the framebuffer.
+            PFD_MAIN_PLANE,
+            0,
+            0, 0, 0
+        };
+
+        HDC ourWindowHandleToDeviceContext = GetDC(hWnd);
+
+        int  letWindowsChooseThisPixelFormat;
+        letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd);
+        SetPixelFormat(ourWindowHandleToDeviceContext, letWindowsChooseThisPixelFormat, &pfd);
+
+        HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
+        wglMakeCurrent(ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
+
+        auto version = gladLoadGL(getproc);
+        if (version == 0) {
+            printf("Failed to initialize OpenGL context\n");
+            return -1;
+        }
+
+        // Successfully loaded OpenGL
+        printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+        MessageBoxA(0, (char*)glGetString(GL_VERSION), "OPENGL VERSION", 0);
+
+        //wglMakeCurrent(ourWindowHandleToDeviceContext, NULL); Unnecessary; wglDeleteContext will make the context not current
+        wglDeleteContext(ourOpenGLRenderingContext);
+        PostQuitMessage(0);
+    }
+    break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+
+}
+int main() {
+    MSG msg = { 0 };
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
+    wc.lpszClassName = L"oglversionchecksample";
+    wc.style = CS_OWNDC;
+    if (!RegisterClass(&wc))
+        return 1;
+    CreateWindowW(wc.lpszClassName, L"openglversioncheck", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 640, 480, 0, 0, wc.hInstance, 0);
+
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+        DispatchMessage(&msg);
+
+    return 0;
 }
